1d4ecc26a598cb90b86ef894e6cfbe0238f0feac
Matthew Conlen <mc@mathisonian.com>
2017-04-09 15:35:21 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# idyll-d3-component
-Component base class to make it easy to integrate d3 into Idyll projects
+# idyll-component
+Extensible component to be used in idyll projects
+
+## Installation
+
+```
+npm install --save idyll-component
+```
+
+
+## Usage
+
+```jsx
+const React = require('react');
+const IDLComponent = require('../idl-component');
+
+class CustomComponent extends IDLComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  handleClick() {
+    ...
+    this.updateProps({
+      prop1: newProp1
+    });
+  }
+
+  render() {
+
+    return (
+      <div prop1={this.props.prop1} >
+        {...}
+      </div>
+    )
+  }
+}
+
+CustomComponent.defaultProps = {
+  ...
+}
+
+module.exports = CustomComponent;
+```
---
+++ index.js
@@ -0,0 +1,38 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const IdyllComponent = require('idyll-component');
+
+class D3Component extends IdyllComponent {
+
+  componentDidMount() {
+    if (super.componentDidMount) {
+      super.componentDidMount();
+    }
+    const node = ReactDOM.findDOMNode(this);
+    this.initialize(node, this.props);
+  }
+
+  initialize() {
+    console.warn('initialize() not defined on D3Component.');
+  }
+
+  update() {
+    console.warn('update() not defined on D3Component.');
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.update(nextProps);
+  }
+
+  shouldComponentUpdate() {
+    return false;
+  }
+
+  render() {
+    const drawNode = this.props.canvas ? 'canvas' : 'svg';
+    const { className, style, width, height } = this.props;
+    return React.createElement(drawNode, { className, style, width, height });
+  }
+}
+
+module.exports = D3Component;
